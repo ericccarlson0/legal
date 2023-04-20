1349eb59bd890b7f9b708dd0721c4b8b45f3d369
Eric Carlson <ecc45@duke.edu>
2023-04-20 11:26:24 -0700

    abstract plaintiff read functions

--- plaintiff/read_america_cuaresma.py
+++ plaintiff/read_america_cuaresma.py
@@ -8,89 +8,102 @@ import util.setup_openai as setup_openai
 
 from plaintiff.plaintiffs import DEPO_DIR, PLA_MAJOR_PROBLEMS_PROMPT
 from pypdf import PdfReader
+from typing import List
 from util.str_util import strip_whitespace, strip_line_numbers, is_numbered, find_first_n, count_spaces
 from util.str_util import string_after, string_before
 from util.token_util import divide_by_tokens
 from util.transcripts import depo_transcript, extract_dotted
 
-reader = PdfReader(os.path.join(DEPO_DIR, "America Cuaresma Full.pdf"))
+def read_pdf(fname):
+    reader = PdfReader(os.path.join(DEPO_DIR, fname))
 
-box = reader.pages[0].mediabox
-print('w', box.width, 'h', box.height)
+    box = reader.pages[0].mediabox
+    # print('w', box.width, 'h', box.height)
 
-l_margin, r_margin = 75, 75
-t_margin, b_margin = 75, 75
+    l_margin, r_margin = 75, 75
+    t_margin, b_margin = 75, 75
 
-body_arr = []
+    body_arr = []
 
-def get_region_visitor(x_min, x_max, y_min, y_max):
-    def visitor(text, cm, tm, font_dict, font_size):
-        _, _, _, _, x, y = tm
-        if y < y_min or y > y_max:
-            return
-        if x < x_min or x > x_max:
-            return
+    def get_region_visitor(x_min, x_max, y_min, y_max):
+        def visitor(text, cm, tm, font_dict, font_size):
+            _, _, _, _, x, y = tm
+            if y < y_min or y > y_max:
+                return
+            if x < x_min or x > x_max:
+                return
+            
+            body_arr.append(extract_dotted(text) if '·' in text else text)
         
-        body_arr.append(extract_dotted(text) if '·' in text else text)
-    
-    return visitor
+        return visitor
 
-page_transcripts = []
-regions = [
-    [l_margin, box.width // 2, box.height // 2, int(box.height - b_margin)],
-    [l_margin, box.width // 2, t_margin, box.height // 2],
-    [box.width // 2, int(box.width - r_margin), box.height // 2, int(box.height - b_margin)],
-    [box.width // 2, int(box.width - r_margin), t_margin, box.height // 2],
-]
-for i, p in enumerate(reader.pages):
-    if i < 1:
-        continue
-
-    for x_min, x_max, y_min, y_max in regions:
-        body_arr.clear()
-        p.extract_text(visitor_text=get_region_visitor(x_min, x_max, y_min, y_max))
-
-        body_arr = strip_whitespace(body_arr)
-        if len(body_arr) == 0:
-            print("\nEMPTY\n")
+    page_transcripts = []
+    regions = [
+        [l_margin, box.width // 2, box.height // 2, int(box.height - b_margin)],
+        [l_margin, box.width // 2, t_margin, box.height // 2],
+        [box.width // 2, int(box.width - r_margin), box.height // 2, int(box.height - b_margin)],
+        [box.width // 2, int(box.width - r_margin), t_margin, box.height // 2],
+    ]
+    for i, p in enumerate(reader.pages):
+        if i < 1:
             continue
 
-        first_n = find_first_n(body_arr)
-        try:
-            body_arr = strip_line_numbers(body_arr, n=first_n)
-        except:
-            print("\nNOT NUMBERED\n")
-            print(body_arr[0])
-            print(body_arr[1], '...\n')
-        body_arr = strip_whitespace(body_arr)
+        for x_min, x_max, y_min, y_max in regions:
+            body_arr.clear()
+            p.extract_text(visitor_text=get_region_visitor(x_min, x_max, y_min, y_max))
 
-        print(i, f'({x_min}, {x_max}, {y_min}, {y_max})')
+            body_arr = strip_whitespace(body_arr)
+            if len(body_arr) == 0:
+                print("\nEMPTY\n")
+                continue
 
-        transcript = ' '.join(body_arr)
-        transcript = re.sub(' +', ' ', transcript)
-        print(len(transcript), 'letters')
-        page_transcripts.append(transcript)
+            first_n = find_first_n(body_arr)
+            try:
+                body_arr = strip_line_numbers(body_arr, n=first_n)
+            except:
+                print("\nNOT NUMBERED")
+                print(body_arr[0])
+                print(body_arr[1], '...\n')
+            body_arr = strip_whitespace(body_arr)
 
-transcript = ' '.join(page_transcripts)
-transcript = re.sub(' +', ' ', transcript)
+            print(i, f'({x_min}, {x_max}, {y_min}, {y_max})')
 
-transcript = string_after(transcript, "P R O C E E D I N G S", include=False)
-transcript = string_before(transcript, "(End of proceedings.)", include=False)
-# print(transcript)
+            transcript = ' '.join(body_arr)
+            transcript = re.sub(' +', ' ', transcript)
+            print(len(transcript), 'letters')
+            page_transcripts.append(transcript)
 
-segments = divide_by_tokens(transcript, 3072)
+    transcript = ' '.join(page_transcripts)
+    transcript = re.sub(' +', ' ', transcript)
 
-for i, s in enumerate(segments):
-    print(i)
+    transcript = string_after(transcript, "P R O C E E D I N G S", include=False)
+    transcript = string_before(transcript, "(End of proceedings.)", include=False)
+    # print(transcript)
 
-    messages = [
-        {"role": "system", "content": PLA_MAJOR_PROBLEMS_PROMPT},
-        {"role": "user", "content": s}
-    ]
+    contents = read_text(transcript)
+    print(contents)
 
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                              messages=messages,
-                                              temperature=.2)
-    print('usage:', completion.usage)
-    content = completion.choices[0].message.content
-    print(content)
+def read_text(input: str) -> List[str]:
+    segments = divide_by_tokens(input, 3072)
+
+    contents = []
+    for i, s in enumerate(segments):
+        print(i)
+
+        messages = [
+            {"role": "system", "content": PLA_MAJOR_PROBLEMS_PROMPT},
+            {"role": "user", "content": s}
+        ]
+
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=messages,
+                                                temperature=.2)
+        print('usage:', completion.usage)
+        content = completion.choices[0].message.content
+        print(content)
+        contents.append(content)
+    
+    return contents
+
+if __name__ == '__main__':
+    read_pdf("America Cuaresma Full.pdf")
