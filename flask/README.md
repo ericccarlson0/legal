f2ad14eecc183f46840b335932aaaace8bc2df77
Eric Carlson <ecc45@duke.edu>
2023-04-21 09:54:30 -0700

    Flask sample app

---
+++ flask/README.md
@@ -0,0 +1,4 @@
+# SAMPLE FLASK APP
+
+A Flask app is not meant to be run from the current directory but rather from the project directory (the directory above it).
+This directory partly exists simply because text/code editors sometimes don't play nice over SSH, and it is nice to keep track over GitHub anyways.
---
+++ flask/main.py
@@ -0,0 +1,55 @@
+import os
+import requests
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from plaintiff.read_america_cuaresma import read_text
+
+app = Flask(__name__)
+CORS(app)
+
+TOKEN = os.environ["SF_CONNECTED_APP_TOKEN"]
+
+SAMPLE_TEXT = """
+Q. All right. Good morning. My name is Robin Ivey. I'm the attorney who is going to be taking your deposition this morning, and I represent the defendant in this lawsuit. Do you understand who I am and who I represent?
+A. No. I don't know whom you are representing.
+Q. Okay. So you understand that you had filed the lawsuit, correct?
+A. Yes.
+Q. Okay. And then that you sued a person, correct?
+A. Yes.
+Q. Okay. So I represent that person.
+A. That's okay.
+Q. All right. So you understand who I am and who I represent?
+A. Yes. Yes. Yes. Now, I understand.
+Q. Okay. Have you ever had your deposition taken before?
+A. No. No.
+Q. Okay. So I'm going to go over a few rules. So, Number One, you understand that you're under oath, right? 
+. Correct. And I understand that perfectly well.
+Q. And you understand that your testimony today would be the same kind of testimony if you were in front of a judge or a jury?
+A. Of course. Yes, of course. Very perfectly well, yes.
+"""
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return ",".join(read_text(SAMPLE_TEXT))
+
+    elif request.method == 'POST':
+        form_data = request.form
+        textblock = [k for k in form_data][0]
+        
+        return ",".join(read_text(textblock))
+    
+@app.route("/<file_id>", methods=['GET', 'POST'])
+def summarize(file_id: str):
+    if request.method == 'GET':
+        headers = {
+            'accept': 'application/json',
+            'Authorization': 'Bearer ' + TOKEN
+        }
+        response = requests.get('https://api.339287139604.genesisapi.com/v1/files?Id=' + file_id,
+                                headers=headers)
+        return response.json()
+    
+    elif request.method == 'POST':
+        raise Exception('POST not supported for summarize by file id (yet).')
